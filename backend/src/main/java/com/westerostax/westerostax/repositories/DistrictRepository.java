ae87f03bf7e45e441aae5aa0739684739c3a60bc
Вячеслав Ткаченко <slavik199916@gmail.com>
2023-06-22 01:02:37 +0300

    tested

--- backend/src/main/java/com/westerostax/westerostax/repositories/DistrictRepository.java
+++ backend/src/main/java/com/westerostax/westerostax/repositories/DistrictRepository.java
@@ -1,11 +1,9 @@
 package com.westerostax.westerostax.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.westerostax.westerostax.entity.District;
 
-@Repository
 public interface DistrictRepository extends JpaRepository<District, Integer> {
 
 }
