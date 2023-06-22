11623990c1b191093e082a2f290d12a246a06536
Вячеслав Ткаченко <slavik199916@gmail.com>
2023-06-22 23:53:04 +0300

    Try Message control, some improvements

---
+++ backend/src/main/java/com/westerostax/westerostax/entity/Message.java
@@ -0,0 +1,76 @@
+package com.westerostax.westerostax.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "thread_id")
+    private MessageThread thread;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "recipient_id")
+    private Lord recipient;
+
+    private String content;
+
+    private LocalDateTime sentAt;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public Lord getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(Lord recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(LocalDateTime sentAt) {
+        this.sentAt = sentAt;
+    }
+
+    public void setThread(MessageThread thread2) {
+    }
+
+	public void setSubject(String subject) {
+	}
+
+    public void setTimestamp(LocalDateTime now) {
+    }
+}
