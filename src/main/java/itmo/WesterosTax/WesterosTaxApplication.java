336c03d052f7216d01ba8191679dfc6c3c257317
Slava Tkachenko <slavik199916@gmail.com>
2023-08-01 23:43:47 +0300

    Revert "Коммиты на неделю"

--- src/main/java/itmo/WesterosTax/WesterosTaxApplication.java
+++ src/main/java/itmo/WesterosTax/WesterosTaxApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity
 @SpringBootApplication
 @EnableJpaRepositories("itmo.WesterosTax.entity")
 @ComponentScan(basePackages = { "itmo.WesterosTax.entity" })
