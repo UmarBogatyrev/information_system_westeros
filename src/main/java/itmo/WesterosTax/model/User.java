62fad535e28ee1b051af95fee5c192d1aa6f8b6f
NE7RUN <slavik199916@gmail.com>
2023-08-02 10:00:46 +0300

    small

--- src/main/java/itmo/WesterosTax/model/User.java
+++ src/main/java/itmo/WesterosTax/model/User.java
@@ -1,12 +1,13 @@
 package itmo.WesterosTax.model;
 
 
-import org.springframework.data.annotation.Id;
+
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
