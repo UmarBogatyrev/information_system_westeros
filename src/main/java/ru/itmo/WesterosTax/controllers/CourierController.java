29bf5b85007764c48cdeb477227b110eea9d045c
HerpDerp <eduardkutermin@gmail.com>
2023-09-27 17:52:25 +0300

    fix

--- src/main/java/ru/itmo/WesterosTax/controllers/CourierController.java
+++ src/main/java/ru/itmo/WesterosTax/controllers/CourierController.java
@@ -41,8 +41,12 @@ public class CourierController {
     public String create(@ModelAttribute("user") User user, Model model) {
         User landowner = userRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
         Iterable<District> districts = landowner.getRegion().getDistricts();
-        Iterable<User> couriers = userRepository.findByLandowner(landowner);
+        List<User> couriers = userRepository.findByLandowner(landowner);
         List<District> districtList = new ArrayList<>();
+        if (couriers.size() == 0) {
+            model.addAttribute("districts", districts);
+            return "landowner/user/Create";
+        }
         for (User courier : couriers) {
             for (District district : districts) {
                 if (courier.getDistrict() != district) {
@@ -67,8 +71,12 @@ public class CourierController {
         }
         if (bindingResultUser.hasErrors()) {
             Iterable<District> districts = landowner.getRegion().getDistricts();
-            Iterable<User> couriers = userRepository.findByLandowner(landowner);
+            List<User> couriers = userRepository.findByLandowner(landowner);
             List<District> districtList = new ArrayList<>();
+            if (couriers.size() == 0) {
+                model.addAttribute("districts", districts);
+                return "landowner/user/Create";
+            }
             for (User courier : couriers) {
                 for (District district : districts) {
                     if (courier.getDistrict() != district) {
